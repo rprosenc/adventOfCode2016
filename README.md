4003889c50d2d11518fda80f553c72a9e2f1ddd7
Robert Prosenc <976156+rprosenc@users.noreply.github.com>
2021-12-13 20:43:11 +0100

    added day 2/part II
    refactored I as well.
    
    solution for I was not that dumb at all.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%86/yellow)](src/day02)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -76,9 +76,9 @@ Both parts: 1.29ms
 
 ```
 Day 02
-Time part 1: 1.08ms
-Time part 2: -
-Both parts: 1.08ms
+Time part 1: 0.95ms
+Time part 2: 1.37ms
+Both parts: 2.3200000000000003ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 3/50
-Total time: 2.37ms
+Total stars: 4/50
+Total time: 3.6100000000000003ms
 ```
 
 <!--/RESULTS-->
--- src/day02/index.js
+++ src/day02/index.js
@@ -8,51 +8,65 @@ const keypad = [
     ["7", "8", "9"]
 ];
 
-const movements = {
-    '1U': false,
-    '1D': '4',
-    '1L': false,
-    '1R': '2',
 
-    '2U': false,
-    '2D': '5',
-    '2L': '1',
-    '2R': '3',
+const moveI = (key, direction) => {
+    const movements = {
+        '1U': false,    '2U': false,    '3U': false,
+        '1D': '4',      '2D': '5',      '3D': '6',
+        '1L': false,    '2L': '1',      '3L': '2',
+        '1R': '2',      '2R': '3',      '3R': false,
 
-    '3U': false,
-    '3D': '6',
-    '3L': '2',
-    '3R': false,
+        '4U': '1',      '5U': '2',      '6U': '3',
+        '4D': '7',      '5D': '8',      '6D': '9',
+        '4L': false,    '5L': '4',      '6L': '5',
+        '4R': '5',      '5R': '6',      '6R': false,
 
-    '4U': '1',
-    '4D': '7',
-    '4L': false,
-    '4R': '5',
+        '7U': '4',      '8U': '5',      '9U': '6',
+        '7D': false,    '8D': false,    '9D': false,
+        '7L': false,    '8L': '7',      '9L': '8',
+        '7R': '8',      '8R': '9',      '9R': false,
+    }
 
-    '5U': '2',
-    '5D': '8',
-    '5L': '4',
-    '5R': '6',
+    if (movements[key+direction]) {
+        return movements[key+direction];
+    }
 
-    '6U': '3',
-    '6D': '9',
-    '6L': '5',
-    '6R': false,
+    return key;
+}
 
-    '7U': '4',
-    '7D': false,
-    '7L': false,
-    '7R': '8',
+const moveII = (key, direction) => {
+    const movements = {
+                                        '1U': false,
+                                        '1L': false,
+                                        '1R': false,
+                                        '1D': '3',
 
-    '8U': '5',
-    '8D': false,
-    '8L': '7',
-    '8R': '9',
+                        '2U': false,    '3U': '1',      '4U': false,
+                        '2L': false,    '3L': '2',      '4L': '3',
+                        '2R': '3',      '3R': '4',      '4R': false,
+                        '2D': '6',      '3D': '7',      '4D': '8',
 
-    '9U': '6',
-    '9D': false,
-    '9L': '8',
-    '9R': false,
+        '5U': false,    '6U': '2',      '7U': '3',      '8U': '4',      '9U': false,
+        '5L': false,    '6L': '5',      '7L': '6',      '8L': '7',      '9L': '8',
+        '5R': '6',      '6R': '7',      '7R': '8',      '8R': '9',      '9R': false,
+        '5D': false,    '6D': 'A',      '7D': 'B',      '8D': 'C',      '9D': false,
+
+                        'AU': '6',      'BU': '7',      'CU': '8',
+                        'AL': false,    'BL': 'A',      'CL': 'B',
+                        'AR': 'B',      'BR': 'C',      'CR': false,
+                        'AD': false,    'BD': 'D',      'CD': false,
+
+                                        'DU': 'B',
+                                        'DL': false,
+                                        'DR': false,
+                                        'DD': false,
+    }
+
+    if (movements[key+direction]) {
+        return movements[key+direction];
+    }
+
+    return key;
 }
 
 const part1 = (rawInput) => {
@@ -63,9 +77,7 @@ const part1 = (rawInput) => {
     key = '5';
     for (let i = 0; i < input.length; i++) {
         for (let j = 0; j < input[i].length; j++) {
-            if (movements[key+input[i][j]]) {
-                key = movements[key+input[i][j]];
-            }
+            key = moveI(key, input[i][j])
         }
         result.push(key);
     }
@@ -76,7 +88,17 @@ const part1 = (rawInput) => {
 const part2 = (rawInput) => {
     const input = parseInput(rawInput);
 
-    return;
+    const result = [];
+    let key;
+    key = '5';
+    for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < input[i].length; j++) {
+            key = moveII(key, input[i][j])
+        }
+        result.push(key);
+    }
+
+    return result.join('');
 };
 
 run({
@@ -93,7 +115,12 @@ UUUUD`, expected: "1985"
     },
     part2: {
         tests: [
-            // { input: ``, expected: "" },
+            {
+                input: `ULL
+RRDDD
+LURDL
+UUUUD`, expected: "5DB3"
+            },
         ],
         solution: part2,
     },
